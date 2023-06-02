d0278d488e3a9d483fd6b6b3241d59205bfd031e
katariya dharmesh <katariyadharmesh658@gmail.com>
2023-06-02 15:13:22 +0530

    signed apk

--- lib/utills/api_constant.dart
+++ lib/utills/api_constant.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
